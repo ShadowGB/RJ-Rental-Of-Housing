1a46b78004efa0000b3cad2876b67ef46d43d1dc
HCJ <78494@runjian.com>
2022-11-16 16:26:52 +0800

    新增第三章文件夹

---
+++ Chapter03/After/RJRentalOfHousing/RJRentalOfHousing.Domain/Price.cs
@@ -0,0 +1,19 @@
+﻿namespace RJRentalOfHousing.Domain
+{
+    public record Price : Money
+    {
+        private Price(decimal amount, string currencyCode, ICurrencyLookup currencyLookup) : base(amount, currencyCode, currencyLookup)
+        {
+            if (amount < 0)
+                throw new ArgumentException("价格不能为负数", nameof(amount));
+        }
+
+        internal Price(decimal amount,string currencyCode):base(amount,new CurrencyDetails { CurrencyCode=currencyCode })
+        {
+
+        }
+
+        public static Price FromDecimal(decimal amount, string currency, ICurrencyLookup currencyLookup)
+            => new Price(amount, currency, currencyLookup);
+    }
+}
---
+++ Chapter03/After/RJRentalOfHousing/RJRentalOfHousing/ApartmentCommandsApi.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static RJRentalOfHousing.Contracts.Apartments;
+
+namespace RJRentalOfHousing
+{
+    [Route("/apartment")]
+    public class ApartmentCommandsApi : Controller
+    {
+        private readonly ApartmentsApplicationService _applicationService;
+
+        public ApartmentCommandsApi(ApartmentsApplicationService applicationService) => _applicationService = applicationService;
+
+        [HttpPost]
+        public async Task<IActionResult> Post(V1.Create request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("area")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.SetArea request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("address")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.SetAddress request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("rent")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.SetRent request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("deposit")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.SetDeposit request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("remark")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.SetRemark request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [Route("publish")]
+        [HttpPut]
+        public async Task<IActionResult> Put(V1.SentForReview request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+    }
+}
